527d399f17774e58e2ad0bd6013dcf655c314c55
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-10-02 22:37:46 -0400

    getting closer

--- sincircle.py
+++ sincircle.py
@@ -6,7 +6,6 @@ class SinCurve(Scene):
         Circ = ParametricFunction(
             lambda u: (np.cos(u), np.sin(u), 0),
             t_range=[0, 2*np.pi],
-            color=BLUE
         )
 
         sinAxes = Axes(
@@ -30,19 +29,46 @@ class SinCurve(Scene):
         group = VGroup(Circ, sinAxes)
         group.move_to(ORIGIN)
 
-        # Draw sin curve on the axes
-        sinCurve = sinAxes.plot(lambda x: np.sin(x), x_range=[0, 2*np.pi], color=RED)
+        theta = ValueTracker(0)
 
-        # TODO: radius should start with one end in center of circle not center of page, fixing this should get the animation working
-        radius = Line((0,0,0), (1,0,0))
+        # Draw sin curve on the axes
+        sinCurve = sinAxes.plot(lambda x: np.sin(x), x_range=[0, 0.001])
+
+        # Draw radius from center of circle
+        radius = Line((-4,0,0), (-3,0,0))
         self.play(Create(radius))
 
-        def tempRadiusUp(mob):
-            mob.become(Line((0,0,0), Circ.get_end()))
-            
-        radius.add_updater(tempRadiusUp)
 
         # Create and animate the objects
         self.play(Create(Circ))
         self.play(Create(sinAxes))
         self.play(Create(sinCurve))
+
+        label1 = Text("angle").scale(.50).next_to(sinAxes, RIGHT)
+        label2 = Text("y - coordinate").scale(.50).next_to(sinAxes, UP).shift(3*LEFT)
+        self.add(label1, label2)
+        self.wait()
+
+        def sinCurveUp(mob):
+            if theta.get_value() > 0:
+                mob.become(sinAxes.plot(lambda x: np.sin(x), x_range=[0, theta.get_value()]))
+
+        sinCurve.add_updater(sinCurveUp)
+
+        def radiusUp(mob):
+            mob.become(Line((-4,0,0), (np.cos(theta.get_value())-4.05, np.sin(theta.get_value()), 0)))
+        radius.add_updater(radiusUp)
+
+        connector = DashedLine(radius.get_end(), sinCurve.get_end()).set_stroke_width(3).set_color(RED)
+        self.add(connector)
+
+        # Update the connector to follow the sin curve
+        def connectorUp(mob):
+            current_theta = theta.get_value()
+            radius_end = (np.cos(current_theta)-4.05, np.sin(current_theta), 0)
+            sin_curve_end = sinAxes.c2p(current_theta, np.sin(current_theta))
+            mob.become(DashedLine(radius_end, sin_curve_end).set_stroke_width(3).set_color(RED))
+        connector.add_updater(connectorUp)
+
+        self.play(theta.animate.set_value(2*np.pi))
+
