9dd3ba1f41fb4f000bfda9caf042f3f53556faad
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-10-05 22:37:10 -0400

    color changes added

--- circgrow.py
+++ circgrow.py
@@ -5,7 +5,15 @@ class CircGrow(Scene):
         circle = Circle(color=BLUE, fill_opacity=0.5, radius = 1)
         self.play(Create(circle))
         self.wait()
-        self.play(circle.animate.scale(2))
+        # Scale up and change to red
+        self.play(circle.animate.scale(2).set_color(RED))
         self.wait()
-        self.play(circle.animate.scale(0.5))
+        # Scale down and change to green
+        self.play(circle.animate.scale(0.5).set_color(GREEN))
+        self.wait()
+        # Scale up again and change to purple
+        self.play(circle.animate.scale(3).set_color(PURPLE))
+        self.wait()
+        # Final scale down and change to orange
+        self.play(circle.animate.scale(0.3).set_color(ORANGE))
         self.wait()
