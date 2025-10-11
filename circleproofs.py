e1f7e4e744cf296cd2d0aeea1b637ac27501248c
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-10-10 23:50:08 -0400

    good improvements with the unit circle creation

--- circleproofs.py
+++ circleproofs.py
@@ -5,13 +5,24 @@ class Circumference(Scene):
     def construct(self):
         # Create axes with specific dimensions
         axes = Axes(
-            x_range=[-2, 2], 
-            y_range=[-2, 2],
-            x_length=6,  # Physical size on screen
-            y_length=6   # Physical size on screen
+            x_range=[-2, 2, 1], 
+            y_range=[-2, 2, 1],
+            x_length=4,  # Physical size on screen
+            y_length=4,   # Physical size on screen
+            tips=False,
+            axis_config={
+                "include_numbers": True
+            }
         )
         self.play(Create(axes))
         self.wait()
+
+        # Add labels
+        x_label = Text("x").next_to(axes.x_axis.get_end(), RIGHT)
+        y_label = Text("y").next_to(axes.y_axis.get_end(), UP)
+        self.play(Create(x_label), Create(y_label))
+        self.wait()
+
 
         # Draw radius of 1
         radius = Line((0,0,0), (1,0,0))
@@ -25,10 +36,6 @@ class Circumference(Scene):
             t_range=[0, 2*np.pi],
             color=BLUE
         )
-
-        # Add labels
-        x_label = Text("x").next_to(axes.x_axis.get_end(), DOWN)
-        y_label = Text("y").next_to(axes.y_axis.get_end(), LEFT)
-        self.play(Create(x_label), Create(y_label))
         self.play(Create(circle))
         self.wait()
+
