f6af5d28aefd34be31a62407b829763315e0dae2
isaiahjon3s <isaiahmarveljones1@gmail.com>
2025-10-12 16:05:48 -0400

    first part of the animation

--- circleproofs.py
+++ circleproofs.py
@@ -15,27 +15,52 @@ class Circumference(Scene):
             }
         )
         self.play(Create(axes))
-        self.wait()
-
         # Add labels
         x_label = Text("x").next_to(axes.x_axis.get_end(), RIGHT)
         y_label = Text("y").next_to(axes.y_axis.get_end(), UP)
         self.play(Create(x_label), Create(y_label))
         self.wait()
 
-
+        # Create theta tracker starting at 0
+        theta = ValueTracker(0)
+        
         # Draw radius of 1
-        radius = Line((0,0,0), (1,0,0))
+        radius = Line((0,0,0), (1,0,0), color=YELLOW)
         self.play(Create(radius))
         self.wait()
-
-        # Create circle with radius 1 using parametric function
-        # This will respect the axes scaling
-        circle = axes.plot_parametric_curve(
-            lambda t: [np.cos(t), np.sin(t)],
-            t_range=[0, 2*np.pi],
+        
+        # Add radius label
+        radius_label = Text("r", color=YELLOW).scale(0.7)
+        radius_label.add_updater(lambda m: m.next_to(radius.get_center(), UP*0.3))
+        self.play(Create(radius_label))
+        self.wait()
+        
+        # Add updater to make radius rotate with theta
+        def radiusUp(mob):
+            mob.become(Line((0,0,0), (np.cos(theta.get_value()), np.sin(theta.get_value()), 0), color=YELLOW))
+        radius.add_updater(radiusUp)
+        
+        # Create circle that will be traced by the radius
+        circle = ParametricFunction(
+            lambda t: (np.cos(t), np.sin(t), 0),
+            t_range=[0, theta.get_value()],  # Start with no circle
             color=BLUE
         )
-        self.play(Create(circle))
+        self.add(circle)
+        
+        # Add updater to make circle grow with theta
+        def circleUp(mob):
+            mob.become(
+                ParametricFunction(
+                    lambda t: (np.cos(t), np.sin(t), 0),
+                    t_range=[0, theta.get_value()],
+                    color=BLUE
+                )
+            )
+        circle.add_updater(circleUp)
+        
+        # Animate theta from 0 to 2*pi to draw the circle
+        self.play(theta.animate.set_value(2*np.pi), run_time=4, rate_func=linear)
         self.wait()
+
 
